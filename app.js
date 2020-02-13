729e3b3ab711ce38233bc26f2f7fea04dd3beeed
yonatan <yonatan.bendahan@gmail.com>
2020-02-13 12:01:10 +0200

    init

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
+
+const Datastore = require('nedb');
+const db = new Datastore({ filename: './storage.db' });
+db.loadDatabase();
+
+function fibonacci(n) {
+  if (n < 2) {
+    return n;
+  }
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+function wait(time) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, time);
+  })
+}
+
+app.get('/fibonacci/:number', async (req, res) => {
+  await wait(600);
+  const number = +req.params.number;
+  if (number === 42) {
+    return res.status(400).send('42 is the meaning of life');
+  }
+  if (number > 50) {
+    return res.status(400).send("number can't be bigger than 50");
+  }
+  if (number < 1) {
+    return res.status(400).send("number can't be smaller than 1");
+  }
+  const result = fibonacci(number);
+  const obj = { number, result, createdDate: Date.now() };
+  db.insert(obj, (err) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send(obj);
+    }
+  });
+});
+
+app.get('/getFibonacciResults', async (req, res) => {
+  await wait(600);
+  db.find({}, (err, docs) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.send({ results: docs });
+    }
+  })
+});
+
+const PORT = 5050;
+app.listen(5050, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
