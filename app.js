e6cf090a60a0532b2a1265dee9e09dc80f8a63ef
Jonatan <jonatan.kruszewski@itc.tech>
2021-02-10 11:25:19 +0200

    HF: Fixed shifting in fibonacci sequence.

--- app.js
+++ app.js
@@ -9,12 +9,9 @@ const db = new Datastore({ filename: "./storage.db" });
 db.loadDatabase();
 
 function fibonacci(n, memo = {}) {
-  if (n in memo) {
-    return memo[n];
-  }
-  if (n <= 1) {
-    return 1;
-  }
+  if (n in memo) return memo[n];
+  if (n === 0) return 0;
+  if (n <= 2) return 1;
   return (memo[n] = fibonacci(n - 1, memo) + fibonacci(n - 2, memo));
 }
 
